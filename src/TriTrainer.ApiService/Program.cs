4d02a9263c0dd6243ede74f0b898e1a37521892d
Hangsolow <1652316+Hangsolow@users.noreply.github.com>
2026-05-15 19:30:10 +0200

    fix(api): prevent JSON cycle serialization by returning DTO responses

--- src/TriTrainer.ApiService/Program.cs
+++ src/TriTrainer.ApiService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using TriTrainer.ApiService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 builder.AddNpgsqlDbContext<ActivitiesDbContext>("tritrainerdb");
 
